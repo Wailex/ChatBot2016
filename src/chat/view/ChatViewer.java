c90318bbdcadac689b4772037f81585f67fd7416
Wailex <mwag0654@740s-pl-103.local>
2016-10-18 08:38:39 -0600

    Added JOptionPane Class.

--- src/chat/view/ChatViewer.java
+++ src/chat/view/ChatViewer.java
@@ -1,5 +1,29 @@
 package chat.view;
+import javax.swing.JOptionPane;
+public class ChatViewer 
+{
 
-public class ChatViewer {
-
+public String collectResponse(String queston)
+{
+	String response = "";
+	reponse = JOptionPane.showInputDialog(null, question);
+	return response;
+}
+	public void displayMessage(String message)
+	{
+	JOptionPane.showMessageDialog(null, message);	
+	}
+	public int collectUserOption(String question)
+	{
+		int response = 0;
+	
+		response = JOptionPane.showConfrimDialog(null, question);
+		
+		return reponse;
+	
+	
+	}
+	
+	
+	
 }
